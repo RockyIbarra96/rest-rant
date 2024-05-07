f61130d5c7bc324cbea9ff991bfea5821c2334d7
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-07 12:39:31 -0700

    adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
+// Modules and Globals
 require('dotenv').config()
-// Require needed modules
 const express = require('express')
-
-// initialize the app object
 const app = express()
 
-//Defining view engine
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-//import places.js
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-//create a homepage route. 
 app.get('/', (req, res) => {
-    //this gets sent to the client
     res.render('home')
 })
 
-//create alternate path
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
-//listen for connections
+// Listen for Connections
 app.listen(process.env.PORT)
