2d50ef176c8f374942c6fa2fe1afa38fce062d6a
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-09 17:50:19 -0700

    commented out console log

--- controllers/places.js
+++ controllers/places.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'https://images.unsplash.com/photo-1497644083578-611b798c60f3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
