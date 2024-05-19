57bff12b8ce3ba633239eb2570fb817ae0d46ba8
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-18 17:46:05 -0700

    added comments to show view

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,27 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>
+                        {c.rant ? 'Rant!' : 'Rave!'}
+                    </h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main className='show-main'>
@@ -25,7 +46,7 @@ function show (data) {
             </section>
             <section>
                 <h2 className='show-comments'>Comments</h2>
-                    <p>No comments yet</p>
+                    {comments}
             </section>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
