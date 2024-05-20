2e5719135995183e406b20d06f9ed4230725edde
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-20 15:02:31 -0700

    configured routes for edit and delete buttons

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -91,11 +91,11 @@ function show (data) {
                     <input type="submit" className="btn btn-primary" value="Add Comment" />
                     </form>
             </section>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
                 Edit
             </a>     
 
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">
                     Delete
                 </button>
