d08871c955155692580eb31713360e6a77499061
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-20 14:20:39 -0700

    added star ratings and functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not Yet Rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length 
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className='border'>
@@ -36,7 +50,7 @@ function show (data) {
                 </div>
                 <div className='rating-description-container'>
                     <h2 className='show-rating'>Rating</h2>
-                        <p>no rating yet</p>
+                        {rating}
                     <h2 className='show-description'>Description</h2>
                         <h3> {data.place.showEstablished()} </h3>
                         <h4>
