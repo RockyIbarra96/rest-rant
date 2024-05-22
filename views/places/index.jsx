f1c69f0f76ef327a0ef5aa86a169a429e78f6915
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-05-22 16:40:30 -0700

    small update

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,9 +2,9 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
-        <div className="col-sm-6">
+        <div className="col-sm-6" key={place.id}>
           <h2 className="text-center">
             <a href={`/places/${place.id}`}>
               {place.name}
